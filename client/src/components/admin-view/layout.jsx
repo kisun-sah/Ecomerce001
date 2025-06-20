ad14d4586aa3c8ba7927f35a6755483ffbf6e418
kisun-sah <kishunsah783@gmail.com>
2025-06-20 14:29:31 +0530

    add new chat

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -2,23 +2,22 @@ import { Outlet } from "react-router-dom";
 import AdminSideBar from "./sidebar";
 import AdminHeader from "./header";
 import { useState } from "react";
+
 function AdminLayout() {
   const [openSidebar, setOpenSidebar] = useState(false); // Control sidebar state
 
   return (
-    <div className=" flex min-h-screen w-full">
+    <div className="flex min-h-screen w-full overflow-hidden">
       {/* Admin Sidebar */}
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
       
-      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar}  />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col w-full">
         {/* Admin Header */}
-      
         <AdminHeader setOpen={setOpenSidebar} />
-       
-        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
-          {/* Your page content */}
-          <Outlet />
 
+        {/* Main Content */}
+        <main className="flex-1 flex flex-col bg-muted/40 p-4 md:p-6 overflow-auto">
+          <Outlet />
         </main>
       </div>
     </div>
@@ -26,7 +25,5 @@ function AdminLayout() {
 }
 
 export default AdminLayout;
-
-
 
 
